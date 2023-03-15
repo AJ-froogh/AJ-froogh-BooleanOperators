e62604f1f990d2f0dc5dd9e3b7845fdcc06e6298
AJ-froogh <ahmadjawid.froogh456@gmail.com>
2023-03-15 20:10:01 +0000

    completed

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -13,7 +13,7 @@ public class Lab {
      * @param b a true/false value.
      * @return if b is true, return true. if b is false, return false.
      */
-    public boolean returnBoolean(boolean b){if(b == true){return true;}
+    public boolean returnBoolean(boolean b){if(b == true){return true; }
         return false;
     }
 
